c7a8e5f08d9c350a6d82e82b4d527131b60f7f21
AdamStajek <adasta146@gmail.com>
2025-01-20 13:43:47 +0100

    numbers analysis

--- analysis/NLP/analysis_classes/numbers_words.py
+++ analysis/NLP/analysis_classes/numbers_words.py
@@ -1,16 +1,18 @@
-from analysis.analysis_base_class import AnalysisBaseClass
-from analysis.NLP.gpt import analyze_speech
-from speech2text.speech2text import Speech2Text
+from analysis.NLP.analysis_nlp_base_class import NlpAnalysisBaseClass
 
-class NumbersDetection(AnalysisBaseClass):
+class NumbersDetection(NlpAnalysisBaseClass):
     def __init__(self, transcript_path):
         super().__init__(transcript_path)
-        self.error_name = "jargon"
-        
-
+        self.error = "numbers"
+    
     def analyze(self):
-        response = analyze_speech("", f"Find numbers in the transcript: {self.transcript_text}")
-        print(response)
-        response = self.add_timestamps(response)
-        return response
+        words = self.transcript_text.split()
+        numbers = [word for word in words if word.isdigit()]
+        return self.add_timestamps(numbers)
+    
+if __name__ == "__main__":
+    numbers = NumbersDetection("data/transcripts/number_example.json")
+    analysis = numbers.analyze()
+    print(analysis)
+    
     
