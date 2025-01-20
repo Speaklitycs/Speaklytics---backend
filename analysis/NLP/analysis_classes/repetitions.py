d5f3ceb223a0f7c4f4d6e13a0269241a1d85b17b
AdamStajek <adasta146@gmail.com>
2025-01-20 14:20:47 +0100

    repetitions analysis

--- analysis/NLP/analysis_classes/repetitions.py
+++ analysis/NLP/analysis_classes/repetitions.py
@@ -1,15 +1,26 @@
-from analysis.analysis_base_class import AnalysisBaseClass
+from analysis.NLP.analysis_nlp_base_class import NlpAnalysisBaseClass
 
-class RepetitionDetection(AnalysisBaseClass):
+class RepetitionsDetection(NlpAnalysisBaseClass):
     def __init__(self, transcript_path):
-        super().__init__(self, RepetitionDetection)
-        self.error_name = "repetitions"
-        with open(transcript_path, "r", encoding="utf-8") as f:
-            self.transcript = f.read()
+        super().__init__(transcript_path)
+        self.error = "repetitions"
+        self.system = self.prompt[self.error]
 
-    def analyze(self):
-        return {
-            "error": self.error_name,
-            "gaps": [
-            ]
+    def add_timestamps(self, errors):
+        response = {
+            "error": self.error,
+            "gaps": []
         }
+
+        for error in errors:
+            for word in self.transcript_with_timestamps["words"]:
+                if word["word"] == error:
+                    response["gaps"].append({"start": word["start"], "end": word["end"]})
+        return response
+    
+if __name__ == "__main__":
+    repetitions = RepetitionsDetection("data/transcripts/repetition_example.json")
+    analysis = repetitions.analyze()
+    print(analysis)
+    
+    
